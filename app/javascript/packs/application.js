c0ea6c389833d881a66273425279a5bf41f25a95
Marcelo Martins <marcelo.martins@cilia.com.br>
2021-08-04 22:55:01 -0300

    adding template

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,9 +12,10 @@ import 'bootstrap'
 import './src/application.scss'
 
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+$(function () {
+  'use strict'
+
+  $('[data-toggle="offcanvas"]').on('click', function () {
+    $('.offcanvas-collapse').toggleClass('open')
+  })
+})
