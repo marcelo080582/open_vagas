8ecc802d1cd665df81c130352f3fa19a37fb252e
Marcelo Martins <marcelo.martins@cilia.com.br>
2021-08-07 23:03:24 -0300

    Create controllers companies, positions and applicants

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
+
+  resources :companies, only: %i[new edit update create]
+  resources :positions
+  resources :applicants, only: %i[index new create]
 end
