b5f0044e0f9ea2eff2056034eba0c8f6a0157c2e
Marcelo Martins <marcelo.martins@cilia.com.br>
2021-08-10 00:04:20 -0300

    Adding action text; improve form position; create action update company

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,9 +1,10 @@
 class Position < ApplicationRecord
   belongs_to :company
+  has_rich_text :description
 
   enum career: [:developer, :business_inteligence, :information_technology, :design,
                 :product, :technology, :other]
   enum contract: [:clt, :pj, :match]
 
-  validates :name, :career, :contract, :city, :state, :summary, :description, presence: true
+  validates :name, :career, :contract, :city, :state, :summary, presence: true
 end
